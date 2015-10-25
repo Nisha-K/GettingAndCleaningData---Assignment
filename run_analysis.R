6e755dedca1d1b8b3e7e6f7cf76bc6be0258eac9
Nisha Kulkarni <nishagtitty@gmail.com>
2015-10-26 01:04:41 +0530

    Get and clean data - code
    assignment code

---
+++ run_analysis.R
@@ -0,0 +1,30 @@
+##rm(list=ls())
+setwd("H:/Coursera - DataScience/03 Getting and Cleaning Data/Assignment")
+
+
+#Loading features and activities
+features <- read.table("UCI HAR Dataset/features.txt", col.names=c("featureId", "featureLabel"))
+feature_label<-features$featureLabel
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names=c("activityId", "activityLabel"))
+head(activities)
+head(features)
+
+##Loading test data
+X_test<-read.table("UCI HAR Dataset/test/X_test.txt")
+Y_test<-read.table("UCI HAR Dataset/test/y_test.txt",col.names="Label")
+subject_test = read.table("UCI HAR Dataset/test/subject_test.txt",col.names="SubjectID")
+colnames(X_test)<-feature_label
+
+##Loading train data
+X_train<-read.table("UCI HAR Dataset/train/X_train.txt")
+Y_train<-read.table("UCI HAR Dataset/train/y_train.txt",col.names="Label")
+subject_train = read.table("UCI HAR Dataset/train/subject_train.txt",col.names="SubjectID")
+colnames(X_train)<-feature_label
+
+#Merging data
+testData<-cbind(subject_test, Y_test, X_test)
+trainData<-cbind(subject_train, Y_train, X_train)
+data <- rbind(testData,trainData)
+
+#Calculating mean and standard deviation
+
